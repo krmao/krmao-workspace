36642db5d8df2c71c48d0237bc2b1dbed255d41d
kr.mao <kr.mao@ctrip.com>
2019-04-16 12:12:24 +0800

    [ios] init code

---
+++ mobile/mobile-smart/mobile-smart-ios/mobile-smart-ios/mobile-smart-ios/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mobile-smart-ios
+//
+//  Created by krmao on 2019/4/16.
+//  Copyright © 2019 smart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ mobile/mobile-smart/mobile-smart-ios/mobile-smart-ios/mobile-smart-ios/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  mobile-smart-ios
+//
+//  Created by krmao on 2019/4/16.
+//  Copyright © 2019 smart. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
