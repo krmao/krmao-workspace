ab629bae9268a7d630a048bb1465d71fc03c368d
kr.mao <kr.mao@ctrip.com>
2019-04-16 17:21:51 +0800

    [ios] create .a success

---
+++ mobile/mobile-smart/mobile-smart-ios/mobile-smart-ios/libraries/library-base/library-base/STBaseUIViewController.m
@@ -0,0 +1,12 @@
+//
+// Created by krmao on 2019-04-16.
+// Copyright (c) 2019 smart. All rights reserved.
+//
+
+#import "STBaseUIViewController.h"
+
+
+@implementation STBaseUIViewController {
+
+}
+@end
---
+++ mobile/mobile-smart/mobile-smart-ios/mobile-smart-ios/libraries/library-base/library-base/library_base.m
@@ -0,0 +1,13 @@
+//
+//  library_base.m
+//  library-base
+//
+//  Created by krmao on 2019/4/16.
+//  Copyright © 2019 smart. All rights reserved.
+//
+
+#import "library_base.h"
+
+@implementation library_base
+
+@end
