3f79ba5cbd47420c83a661dad3b67e5adb16f61a
kr.mao <kr.mao@ctrip.com>
2019-04-16 23:03:48 +0800

    [ios] update

---
+++ mobile/mobile-smart/mobile-smart-ios/dependencies/library-base/library-base/STBaseUIViewController.h
@@ -0,0 +1,10 @@
+//
+// Created by krmao on 2019-04-16.
+// Copyright (c) 2019 smart. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface STBaseUIViewController : UIViewController
+@end
--- mobile/mobile-smart/mobile-smart-ios/mobile-smart-ios/libraries/library-base/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#进入脚本目录
-echo 'start build ...'
-BASEDIR=$(dirname $0)
-cd $BASEDIR
-
-build_cmd='xcodebuild OTHER_CFLAGS="-fembed-bitcode" -target library-base -configuration '${CONFIGURATION}' -sdk '${SDK_NAME}
-echo '执行build命令: '${build_cmd}
-${build_cmd}
