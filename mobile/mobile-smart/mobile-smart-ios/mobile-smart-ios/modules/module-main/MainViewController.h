d5b4549b6b8f9d56966aa54b568d83403c11967d
kr.mao <kr.mao@ctrip.com>
2019-04-16 13:47:27 +0800

    [ios] remove storyboard and use just code to main view controller

---
+++ mobile/mobile-smart/mobile-smart-ios/mobile-smart-ios/modules/module-main/MainViewController.h
@@ -0,0 +1,11 @@
+//
+// Created by krmao on 2019-04-16.
+// Copyright (c) 2019 smart. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+
+@interface MainViewController : UIViewController
+@end
---
+++ mobile/mobile-smart/mobile-smart-ios/mobile-smart-ios/modules/module-main/MainViewController.m
@@ -0,0 +1,39 @@
+//
+// Created by krmao on 2019-04-16.
+// Copyright (c) 2019 smart. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@implementation MainViewController {
+
+
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    self.view.backgroundColor = UIColor.blueColor;
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+}
+
+- (void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+}
+
+- (void)dealloc {
+
+}
+
+@end
